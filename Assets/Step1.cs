fa3a418ef39a2dd0f54c2f686c264ce28d1a9ead
YUNSUNGWOONG <2170004487z@gmail.com>
2023-11-12 16:00:10 +0900

    update 20231112
    update 20231112

---
+++ Assets/Step1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int myInt = 10 + 10;
+
+        double myDouble = 10.0;
+
+        float myFloat = 1.0f;
+
+        bool myBool = false;
+
+        string myString = "Hello world";
+
+        Debug.Log(myInt);
+        //Debug.Log(myString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
